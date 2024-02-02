e584f8e264d4e437568e5ef4800068a9da9ccf77
martinsndifon <martinsndifon@gmail.com>
2024-02-02 10:58:06 +0100

    Optimize database with indexing

--- app/core/models.py
+++ app/core/models.py
@@ -12,6 +12,13 @@ class Provider(models.Model):
     provider_no = models.BigAutoField(primary_key=True)
     provider_name = models.CharField(max_length=255, unique=True)
 
+    class Meta:
+        indexes = [
+            models.Index(fields=["provider_no"]),
+            models.Index(fields=["provider_name"]),
+            models.Index(fields=["provider_no", "provider_name"]),
+        ]
+
     def __str__(self):
         return self.provider_name
 
@@ -25,6 +32,14 @@ class Article(models.Model):
     article_name = models.CharField(max_length=255)
     price = models.IntegerField(null=False, blank=False)
     provider_no = models.ForeignKey(Provider, on_delete=models.CASCADE)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["article_no"]),
+            models.Index(fields=["article_no", "price"]),
+            models.Index(fields=["price"]),
+            models.Index(fields=["provider_no"]),
+        ]
 
     def __str__(self):
         return self.article_name
